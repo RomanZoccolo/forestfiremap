eb087c263ecfcb9ce1b7039f3b2edc405db73c2e
RomanZoccolo <92539643+RomanZoccolo@users.noreply.github.com>
2023-11-22 17:13:18 +0100

    Update scripts.js
    Updated ID for Google Analytics

--- russia/scripts.js
+++ russia/scripts.js
@@ -171,5 +171,5 @@ onDocumentReady(() => {
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-5DQBWLV');
+	})(window,document,'script','dataLayer','G-F2J5BFJZ40');
 });
